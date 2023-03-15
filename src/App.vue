45af42504dd3cc9614946464bd8371e34c4885f6
RobertoSimonini <robertosimonini1@gmail.com>
2023-03-15 16:22:22 +0100

    api call check gone right

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,37 @@
 <script>
 import axios from 'axios';
+import AppHeader from './components/AppHeader.vue';
+const apiUri = 'http://localhost:8000/api/'
+export default {
+  name: 'App',
+  components: {AppHeader},
 
+  data() { 
+    return {
+      projects: []
+    }
+  },
+
+  methods: {
+    fetchProjects(){
+      axios.get(apiUri + 'projects').then(res => {
+        
+        this.projects = res.data;
+        
+      });
+    }
+  },
+  created() {
+    this.fetchProjects();
+  }
+}
 
 </script>
 
 
 <template>
-
+    <AppHeader />
+    
 
 </template>
 
