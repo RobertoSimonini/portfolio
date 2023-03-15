79be08f50af312e62740b9dbe70d83a068e4a8a3
RobertoSimonini <robertosimonini1@gmail.com>
2023-03-15 22:45:28 +0100

    added loader

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,31 @@
 import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
 import ProjectCard from './components/ProjectCard.vue';
+import AppLoader from './components/AppLoader.vue';
 const apiUri = 'http://localhost:8000/api/'
 export default {
   name: 'App',
-  components: {AppHeader, ProjectCard},
+  components: {AppHeader, ProjectCard, AppLoader},
 
   data() { 
     return {
-      projects: []
+      projects: [],
+      isLoading: false
+
     }
   },
 
   methods: {
     fetchProjects(){
+      this.isLoading = true;
       axios.get(apiUri + 'projects').then(res => {
-        
+    
         this.projects = res.data;
         
+      }).catch(err => {
+        console.log(err);
+      }).then(() => {
+        this.isLoading = false;
       });
     }
   },
@@ -32,6 +40,8 @@ export default {
 
 <template>
     <AppHeader />
+    <AppLoader v-if="isLoading"/>
+    <h1 v-else-if="!projects.length" class="text-center">Non è presente nessun progetto.</h1>
     <ProjectCard :projects="projects" />    
 
 </template>
