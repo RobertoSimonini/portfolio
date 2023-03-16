019d034524dd185942df750ced107b7cd5f28ac2
RobertoSimonini <robertosimonini1@gmail.com>
2023-03-16 18:39:53 +0100

    added a 404 page

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    name: 'NotFound'
+}
+
+</script>
+
+<template>
+    <div class="not-found-container d-flex justify-content-center align-items-center text-danger flex-column">
+        <h1>
+            404 - Page not Found
+        </h1>
+        <p>
+            We couldn't find that page, try going to:
+            <router-link to="/" class="ms-2 btn btn-warning">Home</router-link>
+        </p>
+    </div>
+</template>
+
+<style scoped>
+
+.not-found-container {
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+}   
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomePage from '../pages/HomePage.vue';
 import ContactPage from '../pages/ContactPage.vue';
 import ProjectDetails from '../pages/ProjectDetails.vue';
+import NotFoundPage from '../pages/NotFoundPage.vue';
 
 const router = createRouter({
     linkExactActiveClass : 'active',
@@ -11,6 +12,7 @@ const router = createRouter({
         {path:'/',  name: 'home', component: HomePage},
         {path:'/contact',  name: 'contact', component: ContactPage},
         {path:'/projects/:id',  name: 'project-details', component: ProjectDetails},
+        { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFoundPage }
     ]
 });
 
